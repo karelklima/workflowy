e11d21a4dfe888b164ce6852959439d79e7fd7b6
karelklima <karelklima@gmail.com>
2023-05-12 10:45:28 +0200

    Added export of all library types

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
 export { WorkFlowy, WorkFlowy as default } from "./src/workflowy.ts";
-export { Client } from "./src/client.ts";
-export { Document } from "./src/document.ts";
-export type { Operation, TreeItem } from "./src/schema.ts";
+export type * from "./src/client.ts";
+export type * from "./src/document.ts";
+export type * from "./src/schema.ts";
